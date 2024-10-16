7c440d1b4744f4c7297d5a1f3a62eab6331a9983
Chuyi Jiang <157999719+chenchencc33@users.noreply.github.com>
2024-10-15 23:23:03 -0400

    Update __init__.py

--- xuance/torchAgent/agents/policy_gradient/__init__.py
+++ xuance/torchAgent/agents/policy_gradient/__init__.py
@@ -1,3 +1,4 @@
+
 from .pg_agent import PG_Agent
 from .ppg_agent import PPG_Agent
 from .a2c_agent import A2C_Agent
@@ -11,3 +12,4 @@ from .sac_agent import SAC_Agent
 from .sacdis_agent import SACDIS_Agent
 from .td3_agent import TD3_Agent
 from .a2ccb_agent import A2CCB_Agent
+from .cbdsac_agent import CBDSAC_Agent
