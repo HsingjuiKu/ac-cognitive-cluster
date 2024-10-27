be30cddbaffa299d23af2ae8199996fb7b0f27a0
Chuyi Jiang <157999719+chenchencc33@users.noreply.github.com>
2024-10-27 17:55:47 -0400

    Update a2ccb_agent.py

--- xuance/torchAgent/agents/policy_gradient/a2ccb_agent.py
+++ xuance/torchAgent/agents/policy_gradient/a2ccb_agent.py
@@ -85,8 +85,6 @@ class A2CCB_Agent(Agent):
         obs = self.envs.reset()
         for _ in tqdm(range(10000)):
             with torch.no_grad():
-                obs = self._process_observation(obs)
-                print(obs)
                 _, action, _ = self.policy2([obs[0],0])  # 直接使用原始的obs[0]
                 acts = action.stochastic_sample()
                 acts = acts.detach().cpu().numpy()
